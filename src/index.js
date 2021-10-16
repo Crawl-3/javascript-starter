31bad48bac725e1993e815b5e3104d85540a2751
WitecuseDev <92276218+WitecuseDev@users.noreply.github.com>
2021-10-16 11:50:01 -0700

    update log message

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const { version } = require("../package.json");
 
 console.log(`v${version}`);
-console.log("Hello from @witecuse/javascript-starter,");
+console.log("Hello from @witecusedev/javascript-starter!");
 console.log("Do not forget to edit the package.json file and star it the github repository :)");
